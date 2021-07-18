827a7b7b9d1699fa0cfab4eeca3bff47062f0a06
vinay vishwakarma <vinay.myjob@gmail.com>
2021-07-18 20:09:57 +0530

    add javascript , nav modification

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const aboutTab = document.querySelector(".about-tab");
 const   aboutSection = document.querySelector(".about-section");
 aboutTab.addEventListener("click",function(e){
@@ -10,15 +12,38 @@ aboutTab.addEventListener("click",function(e){
 }
 });
 
-
-const bgChange = document.querySelector(".BGchange-area");
 const background1 = document.querySelector(".background1");
 const background2 = document.querySelector(".background2");
-
-
-bgChange.addEventListener("click",function(){
+document.addEventListener("dblclick",function(){
     background1.classList.toggle("hidden");
     background2.classList.toggle("hidden");
 
 });
 
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+       // sweet alert module start 
+        swal({
+            title: "Double Mouse Click to Corner",
+            text: "To change the Background",
+            icon: "success",
+        });
+        // sweet alert module start end
+
+    } else {
+     document.body.style.backgroundColor = "pink";
+    }
+  }
+  
+  var x = window.matchMedia("(min-width: 500px)");
+  myFunction(x); // Call listener function at run time
+
+// jQuery code Start 
+
+// setting navbar active
+$(".navbar li a").on("click",function(e){
+    if($(e.target).hasClass("nav-link") && !$(e.target).hasClass("active")){
+        $(".navbar li a.active").removeClass("active");
+        $(e.target).addClass("active");
+    }
+});
